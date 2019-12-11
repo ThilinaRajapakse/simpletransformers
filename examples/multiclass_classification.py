14a4fcc249262996f001d1cb34b65af922144211
GillesJ <gilles.jacobs92@gmail.com>
2019-12-11 14:37:56 +0100

    Added example scripts in example/ subdirectory.

---
+++ examples/multiclass_classification.py
@@ -0,0 +1,21 @@
+from simpletransformers.classification import ClassificationModel
+import pandas as pd
+
+
+# Train and Evaluation data needs to be in a Pandas Dataframe containing at least two columns. If the Dataframe has a header, it should contain a 'text' and a 'labels' column. If no header is present, the Dataframe should contain at least two columns, with the first column is the text with type str, and the second column in the label with type int.
+train_data = [['Example sentence belonging to class 1', 1], ['Example sentence belonging to class 0', 0], ['Example eval senntence belonging to class 2', 2]]
+train_df = pd.DataFrame(train_data)
+
+eval_data = [['Example eval sentence belonging to class 1', 1], ['Example eval sentence belonging to class 0', 0], ['Example eval senntence belonging to class 2', 2]]
+eval_df = pd.DataFrame(eval_data)
+
+# Create a ClassificationModel
+model = ClassificationModel('bert', 'bert-base-cased', num_labels=3, args={'reprocess_input_data': True, 'overwrite_output_dir': True})
+
+# Train the model
+model.train_model(train_df)
+
+# Evaluate the model
+result, model_outputs, wrong_predictions = model.eval_model(eval_df)
+
+predictions, raw_outputs = model.predict(["Some arbitary sentence"])
