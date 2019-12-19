d295677144223eaf7b3ba0538c0c67c48de75826
ThilinaRajapakse <chaturangarajapakshe@gmail.com>
2019-12-19 15:12:16 +0530

    Added examples directory. Updated readme.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="simpletransformers",
-    version="0.11.2",
+    version="0.12.0",
     author="Thilina Rajapakse",
     author_email="chaturangarajapakshe@gmail.com",
     description="An easy-to-use wrapper library for the Transformers library.",
