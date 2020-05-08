Fix for T5 label masking and torchvision imports

simpletransformers/classification/classification_utils.py
@@ -24,8 +24,13 @@ from collections import Counter
 from io import open
 from multiprocessing import Pool, cpu_count
 
-import torchvision
-import torchvision.transforms as transforms
+try:
+    import torchvision
+    import torchvision.transforms as transforms
+    torchvision_available = True
+except ImportError:
+    torchvision_available = False
+    
 from PIL import Image
 from scipy.stats import pearsonr, spearmanr
 from sklearn.metrics import f1_score, matthews_corrcoef