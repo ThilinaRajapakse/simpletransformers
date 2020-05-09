a7e38aab1198b77e78b444fb84b17a411a868f3d
ThilinaRajapakse <chaturangarajapakshe@gmail.com>
2020-05-09 21:20:55 +0530

    Keep saved models in yelp reviews example

--- examples/text_classification/yelp_reviews_polarity/run_trainers.sh
+++ examples/text_classification/yelp_reviews_polarity/run_trainers.sh
@@ -1,27 +1,20 @@
 rm -r cache_dir
-rm -r outputs
 python train.py electra-small
 
 rm -r cache_dir
-rm -r outputs
 python train.py electra-base
 
 rm -r cache_dir
-rm -r outputs
 python train.py bert
 
 rm -r cache_dir
-rm -r outputs
 python train.py roberta
 
 rm -r cache_dir
-rm -r outputs
 python train.py distilbert
 
 rm -r cache_dir
-rm -r outputs
 python train.py distilroberta
 
 rm -r cache_dir
-rm -r outputs
 python train.py xlnet
