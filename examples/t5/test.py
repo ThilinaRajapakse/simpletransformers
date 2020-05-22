d6b21296b5ec99e06abb250a67197cae95cc36c9
ThilinaRajapakse <chaturangarajapakshe@gmail.com>
2020-05-23 02:29:37 +0530

    added t5 to examples/

---
+++ examples/t5/test.py
@@ -0,0 +1,45 @@
+from simpletransformers.t5 import T5Model
+import pandas as pd
+from pprint import pprint
+
+
+model_args = {
+    "reprocess_input_data": True,
+    "overwrite_output_dir": True,
+    "max_seq_length": 128,
+    "eval_batch_size": 128,
+    "num_train_epochs": 1,
+    "save_eval_checkpoints": False,
+    "use_multiprocessing": False,
+    "num_beams": None,
+    "do_sample": True,
+    "max_length": 50,
+    "top_k": 50,
+    "top_p": 0.95,
+    "num_return_sequences": 3,
+}
+
+model = T5Model("test_outputs_large/best_model", args=model_args)
+
+df = pd.read_csv("data/eval_df.tsv", sep="\t").astype(str)
+preds = model.predict(
+    ["ask_question: " + description for description in df["input_text"].tolist()]
+)
+
+questions = df["target_text"].tolist()
+
+with open("test_outputs_large/generated_questions.txt", "w") as f:
+    for i, desc in enumerate(df["input_text"].tolist()):
+        pprint(desc)
+        pprint(preds[i])
+        print()
+
+        f.write(str(desc) + "\n\n")
+
+        f.write("Real question:\n")
+        f.write(questions[i] + "\n\n")
+
+        f.write("Generated questions:\n")
+        for pred in preds[i]:
+            f.write(str(pred) + "\n")
+        f.write("________________________________________________________________________________\n")
---
+++ examples/t5/train.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+from simpletransformers.t5 import T5Model
+
+
+train_df = pd.read_csv("data/train_df.tsv", sep="\t").astype(str)
+eval_df = pd.read_csv("data/eval_df.tsv", sep="\t").astype(str)
+
+model_args = {
+    "reprocess_input_data": True,
+    "overwrite_output_dir": True,
+    "max_seq_length": 128,
+    "train_batch_size": 8,
+    "num_train_epochs": 1,
+    "save_eval_checkpoints": True,
+    "save_steps": -1,
+    "use_multiprocessing": False,
+    # "silent": True,
+    "evaluate_during_training": True,
+    "evaluate_during_training_steps": 15000,
+    "evaluate_during_training_verbose": True,
+    "fp16": False,
+
+    "wandb_project": "Question Generation with T5",
+}
+
+model = T5Model("t5-large", args=model_args)
+
+model.train_model(train_df, eval_data=eval_df)
