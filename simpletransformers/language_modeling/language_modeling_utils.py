03eb111b20ca6a68a644f09cd0a670d5cc76bece
Pradhy729 <pradhy.ramesh@gmail.com>
2020-06-15 20:35:21 -0700

    Cleaned up language_modeling_utils.py
    LineByLineDataset and TextDatasets from this are not being used.
    Looks like they've switched to huggingface tokenizers.

--- simpletransformers/language_modeling/language_modeling_utils.py
+++ simpletransformers/language_modeling/language_modeling_utils.py
@@ -7,118 +7,12 @@ from typing import Tuple
 from tqdm.auto import tqdm
 
 import torch
-from tokenizers.implementations import ByteLevelBPETokenizer
 from tokenizers.processors import BertProcessing
 from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizer
 
 
 logger = logging.getLogger(__name__)
-
-
-class TextDataset(Dataset):
-    def __init__(self, tokenizer: PreTrainedTokenizer, args, file_path: str, mode, block_size=512):
-        assert os.path.isfile(file_path)
-
-        block_size = block_size - (tokenizer.max_len - tokenizer.max_len_single_sentence)
-
-        directory, filename = os.path.split(file_path)
-        cached_features_file = os.path.join(
-            args["cache_dir"], args["model_type"] + "_cached_lm_" + str(block_size) + "_" + filename
-        )
-
-        if os.path.exists(cached_features_file) and (
-            (not args["reprocess_input_data"] and not args["no_cache"])
-            or (mode == "dev" and args["use_cached_eval_features"] and not args["no_cache"])
-        ):
-            logger.info(" Loading features from cached file %s", cached_features_file)
-            with open(cached_features_file, "rb") as handle:
-                self.examples = pickle.load(handle)
-        else:
-            logger.info(" Creating features from dataset file at %s", args["cache_dir"])
-
-            self.examples = []
-            with open(file_path, encoding="utf-8") as f:
-                text = f.read()
-
-            # tokenizer = ByteLevelBPETokenizer(
-            #     "outputs/vocab.json",
-            #     "outputs/merges.txt",
-            # )
-            # tokenizer._tokenizer.post_processor = BertProcessing(
-            #     ("</s>", tokenizer.token_to_id("</s>")),
-            #     ("<s>", tokenizer.token_to_id("<s>")),
-            # )
-
-            # logger.info(" Encoding")
-            # tokenized_text = tokenizer.encode(text).ids
-            # logger.info(" Encoded")
-            # self.examples = [tokenized_text[i : i + block_size] for i in tqdm(range(0, len(tokenized_text) - block_size + 1, block_size))] # noqa
-
-            tokenized_text = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(text))
-            tokenized_text_split = [
-                tokenized_text[i : i + block_size]
-                for i in tqdm(range(0, len(tokenized_text) - block_size + 1, block_size))
-            ]
-
-            with Pool(args["process_count"]) as p:
-                self.examples = list(
-                    tqdm(
-                        p.imap(
-                            tokenizer.build_inputs_with_special_tokens,
-                            tokenized_text_split,
-                            chunksize=args["multiprocessing_chunksize"],
-                        ),
-                        total=len(tokenized_text_split),
-                        # disable=silent,
-                    )
-                )
-
-            # for i in range(0, len(tokenized_text) - block_size + 1, block_size):  # Truncate in block of block_size
-            #     self.examples.append(tokenizer.build_inputs_with_special_tokens(tokenized_text[i : i + block_size]))
-            # Note that we are loosing the last truncated example here for the sake of simplicity (no padding)
-            # If your dataset is small, first you should loook for a bigger one :-) and second you
-            # can change this behavior by adding (model specific) padding.
-
-            logger.info(" Saving features into cached file %s", cached_features_file)
-            with open(cached_features_file, "wb") as handle:
-                pickle.dump(self.examples, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-    def __len__(self):
-        return len(self.examples)
-
-    def __getitem__(self, item):
-        return torch.tensor(self.examples[item], dtype=torch.long)
-
-
-class LineByLineTextDataset(Dataset):
-    def __init__(self, tokenizer: PreTrainedTokenizer, args, file_path: str, block_size=512):
-        assert os.path.isfile(file_path)
-        # Here, we do not cache the features, operating under the assumption
-        # that we will soon use fast multithreaded tokenizers from the
-        # `tokenizers` repo everywhere =)
-        logger.info(" Creating features from dataset file at %s", file_path)
-
-        with open(file_path, encoding="utf-8") as f:
-            lines = [line for line in f.read().splitlines() if (len(line) > 0 and not line.isspace())]
-
-        tokenizer = ByteLevelBPETokenizer(
-            f"{args['tokenizer_name']}/vocab.json", f"{args['tokenizer_name']}/merges.txt",
-        )
-        tokenizer._tokenizer.post_processor = BertProcessing(
-            ("</s>", tokenizer.token_to_id("</s>")), ("<s>", tokenizer.token_to_id("<s>")),
-        )
-
-        tokenizer.enable_truncation(max_length=block_size)
-        self.examples = [t.ids for t in tokenizer.encode_batch(lines)]
-
-        # self.examples = tokenizer.batch_encode_plus(lines, add_special_tokens=True, max_length=block_size)["input_ids"] # noqa
-
-    def __len__(self):
-        return len(self.examples)
-
-    def __getitem__(self, i):
-        return torch.tensor(self.examples[i], dtype=torch.long)
 
 
 def encode(data):
