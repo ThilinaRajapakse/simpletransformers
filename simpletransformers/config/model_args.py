4dae5ac9b2f0f1213e2a547fb1a527dac849777b
Karthik Ganesan <karthikganesan17@gmail.com>
2020-08-29 15:35:36 +0530

    add boolean arg dynamic_quantize

--- simpletransformers/config/model_args.py
+++ simpletransformers/config/model_args.py
@@ -76,6 +76,8 @@ class ModelArgs:
     warmup_ratio: float = 0.06
     warmup_steps: int = 0
     weight_decay: int = 0
+    dynamic_quantize: bool = False
+    
 
     def update_from_dict(self, new_values):
         if isinstance(new_values, dict):
