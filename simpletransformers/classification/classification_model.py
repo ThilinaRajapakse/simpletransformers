43451725726e714d52f0a6ddcf127af227f62d9e
Erik Fäßler <chew@gmx.net>
2020-11-19 12:55:07 +0100

    Only doing `wandb.init()` when there is a sweep
    As determined by `wandb.setup().settings.sweep_id`.

--- simpletransformers/classification/classification_model.py
+++ simpletransformers/classification/classification_model.py
@@ -537,7 +537,7 @@ class ClassificationModel:
             training_progress_scores = self._create_training_progress_scores(multi_label, **kwargs)
 
         if args.wandb_project:
-            if "sweep_config" not in kwargs:
+            if not wandb.setup().settings.sweep_id:
                 logger.info(" Initializing WandB run.")
                 wandb.init(project=args.wandb_project, config={**asdict(args)}, **args.wandb_kwargs)
             wandb.watch(self.model)
