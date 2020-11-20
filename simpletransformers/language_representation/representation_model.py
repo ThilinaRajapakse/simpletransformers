5f205550bae759fa205d720886021b838cabd47f
rgoular <rgoular@us.ibm.com>
2020-11-20 15:30:26 -0500

    Adding movement to selected device

--- simpletransformers/language_representation/representation_model.py
+++ simpletransformers/language_representation/representation_model.py
@@ -136,10 +136,14 @@ class RepresentationModel:
         :param batch_size
         :return: list of lists of sentence embeddings(if `combine_strategy=None`) OR list of sentence embeddings(if `combine_strategy!=None`)
         """
+
+        self.model.to(self.device)
+
         batches = batch_iterable(text_list, batch_size=batch_size)
         embeddings = np.array([])
         for batch in batches:
             input_ids_tensor = self._tokenize(batch)
+            input_ids_tensor = input_ids_tensor.to(self.device)
             token_vectors = self.model(input_ids=input_ids_tensor)
             if combine_strategy:
                 embedding_func_mapping = {"mean": mean_across_all_tokens, "concat": concat_all_tokens}
@@ -149,13 +153,13 @@ class RepresentationModel:
                     raise ValueError(
                         "Provided combine_strategy is not valid." "supported values are: 'concat', 'mean' and None."
                     )
-                batch_embeddings = embedding_func(token_vectors).detach().numpy()
+                batch_embeddings = embedding_func(token_vectors).detach() 
             else:
-                batch_embeddings = token_vectors.detach().numpy()
+                batch_embeddings = token_vectors.detach()
             if len(embeddings) == 0:
-                embeddings = batch_embeddings
+                embeddings = batch_embeddings.cpu().detach().numpy()
             else:
-                embeddings = np.concatenate((embeddings, batch_embeddings), axis=0)
+                embeddings = np.concatenate((embeddings, batch_embeddings.cpu().detach().numpy()), axis=0)
 
         return embeddings
 
