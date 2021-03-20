197631f86ea0a1632e8135b724b203c7871b082c
Thilina Rajapakse <thilinarajapakshe@gmail.com>
2021-03-20 13:31:23 +0530

    Added datasets dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="simpletransformers",
-    version="0.61.0",
+    version="0.61.1",
     author="Thilina Rajapakse",
     author_email="chaturangarajapakshe@gmail.com",
     description="An easy-to-use wrapper library for the Transformers library.",
@@ -27,6 +27,7 @@ setup(
         "tqdm>=4.47.0",
         "regex",
         "transformers>=4.2.0",
+        "datasets",
         "scipy",
         "scikit-learn",
         "seqeval",
