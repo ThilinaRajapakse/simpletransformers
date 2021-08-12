8e79d69152dd47b024691c029cc7559f81d47e32
Dima <zhylko.dima@gmail.com>
2021-08-12 03:52:01 +0200

    rewrite focal_loss

--- simpletransformers/losses/focal_loss.py
+++ simpletransformers/losses/focal_loss.py
@@ -1,5 +1,7 @@
-from typing import Optional
-
+from typing import Optional, Union
+from collections import Iterable
+from numbers import Real
+import warnings
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -18,15 +20,18 @@ class FocalLoss(nn.Module):
     where:
        - :math:`p_t` is the model's estimated probability for each class.
     Arguments:
-        alpha (float): Weighting factor :math:`\alpha \in [0, 1]`.
-        gamma (float): Focusing parameter :math:`\gamma >= 0`.
+        alpha (float): Weighting factor :math:`\alpha \in [0, 1]` for one-vs-others mode (weight of negative class)
+                        or :math:`\alpha_i \in \R`
+                        vector of weights for each class (analogous to weight argument for CrossEntropyLoss)
+        gamma (float): Focusing parameter :math:`\gamma >= 0`. When 0 is equal to CrossEntropyLoss
         reduction (Optional[str]): Specifies the reduction to apply to the
-         output: ‘none’ | ‘mean’ | ‘sum’. ‘none’: no reduction will be applied,
+         output: ‘none’ | ‘mean’ | ‘sum’.
+         ‘none’: no reduction will be applied,
          ‘mean’: the sum of the output will be divided by the number of elements
-         in the output, ‘sum’: the output will be summed. Default: ‘none’.
+                in the output, uses geometric mean if alpha set to list of weights
+         ‘sum’: the output will be summed. Default: ‘none’.
         ignore_index (Optional[int]): specifies indexes that are ignored during loss calculation
          (identical to PyTorch's CrossEntropyLoss 'ignore_index' parameter). Default: -100
-        eps (Optional[float]): small constant to maintain numerical stability of loss calculation. Default: 1e-6
 
     Shape:
         - Input: :math:`(N, C)` where C = number of classes.
@@ -44,16 +49,20 @@ class FocalLoss(nn.Module):
         [1] https://arxiv.org/abs/1708.02002
     """
 
-    def __init__(self, alpha: float,
-                 gamma: float = 2.0,
-                 reduction: Optional[str] = 'mean',
-                 ignore_index: int = -100,
-                 eps: float = 1e-6) -> None:
+    def __init__(self, alpha: Optional[Union[float, Iterable]] = None,
+                 gamma: Real = 2.0,
+                 reduction: str = 'mean',
+                 ignore_index: int = -100) -> None:
         super(FocalLoss, self).__init__()
-        self.alpha: float = alpha
-        self.gamma: float = gamma
-        self.reduction: Optional[str] = reduction
-        self.eps: float = eps
+        if alpha is not None and not isinstance(alpha, float) and not isinstance(alpha, Iterable):
+            raise ValueError(f"alpha value should be None, float value or list of real values. Got: {type(alpha)}")
+        self.alpha: Optional[Union[float, torch.Tensor]] = \
+            alpha if alpha is None or isinstance(alpha, float) else torch.FloatTensor(alpha)
+        if isinstance(alpha, float) and not 0. <= alpha <= 1.:
+            warnings.warn('[Focal Loss] alpha value is to high must be between [0, 1]')
+
+        self.gamma: Real = gamma
+        self.reduction: str = reduction
         self.ignore_index: int = ignore_index
 
     def forward(
@@ -63,49 +72,46 @@ class FocalLoss(nn.Module):
         if not torch.is_tensor(input):
             raise TypeError("Input type is not a torch.Tensor. Got {}"
                             .format(type(input)))
-        if len(input.shape) == 4:
-            #raise ValueError("Invalid input shape, we expect BxNxHxW. Got: {}"
-            #                 .format(input.shape))
-            if input.shape[-2:] != target.shape[-2:]:
-                raise ValueError("input and target shapes must have same 2 last dimensions. Got: {}"
-                                 .format(input.shape, input.shape))
-        elif len(input.shape) == 2:
-            if input.shape[0] != target.shape[0]:
-                raise ValueError("number of elements in input and target shapes must be the same. Got: {}"
-                                 .format(input.shape, input.shape))
-        else:
-            raise ValueError("Invalid input shape, we expect BxNxHxW or BxN. Got: {}"
-                             .format(input.shape))
-        if not input.device == target.device:
+        if input.shape[0] != target.shape[0]:
+            raise ValueError(f"First dimension of inputs and targets should be same shape. "
+                             f"Got: {input.shape} and {target.shape}")
+        if len(input.shape) != 2 or len(target.shape) != 1:
+            raise ValueError(f"input tensors should be of shape (N, C) and (N,). "
+                             f"Got: {input.shape} and {target.shape}")
+        if input.device != target.device:
             raise ValueError(
                 "input and target must be in the same device. Got: {}" .format(
                     input.device, target.device))
-        # compute softmax over the classes axis
-        input_soft = F.softmax(input, dim=1) + self.eps
 
         # filter labels
         target = target.type(torch.long)
         input_mask = target != self.ignore_index
-
         target = target[input_mask]
-        input_soft = input_soft[input_mask]
+        input = input[input_mask]
+        # compute softmax over the classes axis
+        pt = F.softmax(input, dim=1)
+        logpt = F.log_softmax(input, dim=1)
 
-        # create the labels one hot tensor
-        target_one_hot = F.one_hot(target, num_classes=input.shape[1]) \
-            .to(input.device).type(input_soft.dtype)
+        # compute focal loss
+        pt = pt.gather(1, target.unsqueeze(-1)).squeeze()
+        logpt = logpt.gather(1, target.unsqueeze(-1)).squeeze()
+        focal_loss = -1 * (1-pt) ** self.gamma * logpt
 
-        # compute the actual focal loss
-        weight = torch.pow(1. - input_soft, self.gamma)
-        focal = -self.alpha * weight * torch.log(input_soft)
-        loss_tmp = torch.sum(target_one_hot * focal, dim=1)
+        weights = torch.ones_like(focal_loss, dtype=focal_loss.dtype, device=focal_loss.device)
+        if self.alpha is not None:
+            if isinstance(self.alpha, float):
+                weights = torch.where(target > 0, 1-self.alpha, self.alpha)
+            elif torch.is_tensor(self.alpha):
+                self.alpha.to(input.device)
+                weights = self.alpha.gather(0, target)
 
-        loss = -1
+        tmp_loss = focal_loss * weights
         if self.reduction == 'none':
-            loss = loss_tmp
+            loss = tmp_loss
         elif self.reduction == 'mean':
-            loss = torch.mean(loss_tmp)
+            loss = tmp_loss.sum() / weights.sum() if torch.is_tensor(self.alpha) else torch.mean(tmp_loss)
         elif self.reduction == 'sum':
-            loss = torch.sum(loss_tmp)
+            loss = tmp_loss.sum()
         else:
             raise NotImplementedError("Invalid reduction mode: {}"
                                       .format(self.reduction))
