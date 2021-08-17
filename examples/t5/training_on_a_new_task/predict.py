34410b22a676a2a30048ed6b9beaaceecd597cee
mataney <mataneyal1@gmail.com>
2021-08-17 14:16:31 +0300

    Update predict.py
    Change `use_multiprocessed` argument to `use_multiprocessed_decoding`.
    T5Model should receive type of model.
    Too many new lines for `query`.

--- examples/t5/training_on_a_new_task/predict.py
+++ examples/t5/training_on_a_new_task/predict.py
@@ -7,7 +7,7 @@ model_args = {
     "eval_batch_size": 16,
     "num_train_epochs": 1,
     "save_eval_checkpoints": False,
-    "use_multiprocessing": False,
+    "use_multiprocessed_decoding": False,
     # "silent": True,
     "num_beams": None,
     "do_sample": True,
@@ -17,12 +17,11 @@ model_args = {
     "num_return_sequences": 3,
 }
 
-model = T5Model("outputs/best_model", args=model_args)
+model = T5Model("t5", "outputs/best_model", args=model_args)
 
 query = (
-    "ask_question: "
-    + """
-ANTIQUE CAST METAL 3 GLOBE CANDLABRA JADITE LAMP.
+    "ask_question: "\
+    """ANTIQUE CAST METAL 3 GLOBE CANDLABRA JADITE LAMP.
 
 Stunning antique lamp with three candle style globes. Cast metal base with jadite green glass insert. Has been rewired with new braided cord. In excellent condition with only one chip (as pictured) on the edge of the glass insert. E9 69 on underside of metal base. Missing finial. New low wattage globes.
 """
