c0c8f27aaa7b0e211d70009f43bfddd71a028136
Joel Niklaus <joel.niklaus@thomsonreuters.com>
2022-05-31 12:31:07 +0200

    improved data preparation script

--- examples/language_modeling/prepare_data.py
+++ examples/language_modeling/prepare_data.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from datasets import load_dataset
 
 
-def prepare_data(overwrite_cache=False):
+def prepare_data(overwrite_cache=False, debug=False):
     folder = "data"
     if Path(folder).exists() and not overwrite_cache:
         print("data folder already exists. Set the flag overwrite_cache to True to download the data again.")
@@ -15,9 +15,11 @@ def prepare_data(overwrite_cache=False):
 
     for split in ['train', 'test']:
         text = dataset[split]['context']
+        if debug:
+            text = text[:100]
 
         with open(f"data/{split}.txt", 'w+') as output:
-            for row in text[:10]:
+            for row in text:
                 row = re.sub(r'\n+', '\n', row).strip()
                 output.write(str(row) + '\n')
     print(f"Saved the data to the folder {folder}")
