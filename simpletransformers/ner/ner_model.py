b6551edeb986415cc1a50901d3e53848f5cf045c
Thilina Rajapakse <thilinarajapakshe@gmail.com>
2022-09-18 19:00:53 +0200

    Pass betas to adamw

--- simpletransformers/ner/ner_model.py
+++ simpletransformers/ner/ner_model.py
@@ -630,6 +630,7 @@ class NERModel:
                 optimizer_grouped_parameters,
                 lr=args.learning_rate,
                 eps=args.adam_epsilon,
+                betas=args.adam_betas,
             )
         elif args.optimizer == "Adafactor":
             optimizer = Adafactor(
